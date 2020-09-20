8cdf587d4f8c263d4ab7d4f289a877e6fb9d4fa2
Valentin Palkovič <external.valentin.palkovic@kuehne-nagel.com>
2020-09-20 16:44:48 +0200

    test: Write test case to cover dmmf convert

--- src/tests/generator.test.ts
+++ src/tests/generator.test.ts
@@ -1,26 +1,49 @@
 import { getDMMF } from '@prisma/sdk'
 import { transformDMMF } from '../generator/transformDMMF'
-import util from 'util'
 
 const datamodel = /* Prisma */ `
 	model User {
 		id        Int      @id @default(autoincrement())
-		// createdAt DateTime @default(now())
-		// email     String   @unique
-		// name      String?
-		// posts     Post[]
+		createdAt DateTime @default(now())
+		email     String   @unique
+		name      String?
+		posts     Post[]
 	}
 
-	// model Post {
-	// 	id     Int   @id @default(autoincrement())
-	// 	User   User? @relation(fields: [userId], references: [id])
-	// 	userId Int?
-	// }
+	model Post {
+		id     Int   @id @default(autoincrement())
+		User   User? @relation(fields: [userId], references: [id])
+		userId Int?
+	}
 `
 
-describe('TOC', () => {
-    it('renders TOC Subheader correctly', async () => {
+describe('JSON Schema Generator', () => {
+    it('returns JSON Schema for given models', async () => {
         const dmmf = await getDMMF({ datamodel })
-        console.log(util.inspect(dmmf, { showHidden: false, depth: null }))
+        expect(transformDMMF(dmmf)).toEqual({
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            definitions: {
+                Post: {
+                    properties: {
+                        User: { type: 'object' },
+                        id: { type: 'integer' },
+                        userId: { type: 'integer' },
+                    },
+                    required: ['id'],
+                    type: 'object',
+                },
+                User: {
+                    properties: {
+                        createdAt: { format: 'date-time', type: 'string' },
+                        email: { type: 'string' },
+                        id: { type: 'integer' },
+                        name: { type: 'string' },
+                        posts: { type: 'array' },
+                    },
+                    required: ['id', 'createdAt', 'email'],
+                    type: 'object',
+                },
+            },
+        })
     })
 })
